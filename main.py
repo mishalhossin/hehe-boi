33ff15ff391e7359a3bc2ca219efad81cf9c9df7
Mishal <mishalhossin20@gmail.com>
2008-03-09 16:26:23 +0600

    First commit for the day!

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ def makeCommits (days : int):
         # commit 
         os.system('git commit --date="'+ dates +'" -m "First commit for the day!"')
 
-        return days * makeCommits(days - 9)
+        return days * makeCommits(days - 1)
 
-makeCommits(99999999999)
+makeCommits(9999999999999)
